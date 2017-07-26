b5b6c53b4cc980fa275251a83ca34a2141b42508
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-07-25 21:05:47 -0700

    Build against `dhall-1.5.0

--- dhall.nix
+++ dhall.nix
@@ -1,25 +1,23 @@
 { mkDerivation, ansi-wl-pprint, base, bytestring, case-insensitive
-, charset, containers, http-client, http-client-tls, lens
-, neat-interpolation, optparse-generic, parsers, stdenv
-, system-fileio, system-filepath, tasty, tasty-hunit, text
-, text-format, transformers, trifecta, unordered-containers, vector
+, charset, containers, contravariant, http-client, http-client-tls
+, lens, optparse-generic, parsers, stdenv, system-fileio
+, system-filepath, tasty, tasty-hunit, text, text-format
+, transformers, trifecta, unordered-containers, vector
 }:
 mkDerivation {
   pname = "dhall";
-  version = "1.4.2";
-  sha256 = "0wnfqm0478h9fqav13q6fqnj8fzbhigsndnasr0hbcjd3s3qvf0d";
+  version = "1.5.0";
+  sha256 = "13s98jjhibm9p0hd9y9fbj0a1il4mwcp2v9mi9j0zrpn6vr4h00p";
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     ansi-wl-pprint base bytestring case-insensitive charset containers
-    http-client http-client-tls lens neat-interpolation parsers
+    contravariant http-client http-client-tls lens parsers
     system-fileio system-filepath text text-format transformers
     trifecta unordered-containers vector
   ];
   executableHaskellDepends = [ base optparse-generic text trifecta ];
-  testHaskellDepends = [
-    base neat-interpolation tasty tasty-hunit text vector
-  ];
+  testHaskellDepends = [ base tasty tasty-hunit text vector ];
   description = "A configuration language guaranteed to terminate";
   license = stdenv.lib.licenses.bsd3;
 }
