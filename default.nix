87310823ceff4fc3599b4966e4a57bdd3f46d530
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-01-27 14:19:21 -0800

    Version 1.0.4 → 1.0.5

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, dhall, optparse-generic, stdenv, text }:
 mkDerivation {
   pname = "dhall-text";
-  version = "1.0.4";
+  version = "1.0.5";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
